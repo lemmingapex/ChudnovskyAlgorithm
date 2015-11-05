6edf90de300d30da249dc79bcca40fdb6eb13a4b
Scott Wiedemann <lemmingapex@gmail.com>
2015-11-05 12:11:19 -0700

    Travis tests have a non-helpful timeout...

--- src/test/java/sw/chudnovskyalgorithm/ChudnovskyAlgorithmThreadedTest.java
+++ src/test/java/sw/chudnovskyalgorithm/ChudnovskyAlgorithmThreadedTest.java
@@ -25,7 +25,7 @@ public class ChudnovskyAlgorithmThreadedTest {
 
     @Test
     public void testCompareSingleToMultiThreaded() {
-        for (long precision = 1; precision <= Math.pow(2, 17); precision *= 2l) {
+        for (long precision = 1; precision <= Math.pow(2, 15); precision *= 2l) {
 
             long startTime = System.nanoTime();
             Apfloat singleThreadedPi = ChudnovskyAlgorithm.calculatePi(precision);
